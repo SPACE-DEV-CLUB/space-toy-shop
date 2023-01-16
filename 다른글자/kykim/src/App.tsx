b2a1b124d3162e416bae71b90ca9b2c5d6d5c7ca
kykim00 <jeongsaeki@likelion.org>
2023-01-16 20:35:07 +0900

    implement stage movement

--- 다른글자/kykim/src/App.tsx
+++ 다른글자/kykim/src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './Board';
 
 const App = () => {
+  const [stage, setStage] = useState(1);
+
+  const onClcikCorrentAnswer = () => {
+    setStage(prev => prev + 1);
+  };
+
+  const onClickWrongAnswer = () => {
+    setStage(1);
+  };
   return (
     <>
       <h1>다른 글자 찾기</h1>
-      <Board />
+      <h3>{stage} 단계</h3>
+      <Board stage={stage} onCorrect={onClcikCorrentAnswer} onIncorrect={onClickWrongAnswer} />
     </>
   );
 };
--- 다른글자/kykim/src/Board.tsx
+++ 다른글자/kykim/src/Board.tsx
@@ -3,16 +3,27 @@ import getBoard from './utils/getBoard';
 
 interface BoardProps {
   stage?: number;
+  onCorrect?: () => void;
+  onIncorrect?: () => void;
 }
-const Board = ({ stage = 1 }: BoardProps) => {
+const Board = ({ stage = 1, onCorrect = () => {}, onIncorrect = () => {} }: BoardProps) => {
   const board = getBoard(stage);
 
+  const onClickTile = (tileText: string) => {
+    if (tileText === '게절') {
+      onCorrect();
+    } else {
+      onIncorrect();
+    }
+  };
   return (
     <>
       {board.map((tileRow, indexA) => (
         <div key={indexA}>
           {tileRow.map((tile, indexB) => (
-            <li key={`${indexA}_${indexB}`}>{tile}</li>
+            <li key={`${indexA}_${indexB}`} onClick={() => onClickTile(tile)}>
+              {tile}
+            </li>
           ))}
         </div>
       ))}
